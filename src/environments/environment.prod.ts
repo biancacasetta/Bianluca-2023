75c070ea1f3a086e8c8ed296f19aa93dc1aead1d
Bianca Casetta <biancacasetta@gmail.com>
2023-06-10 02:07:24 -0300

    login parcial y creacion proyecto firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'bianluca-pps',
+    appId: '1:123281409180:web:6b3912e3ce15e4479c9959',
+    storageBucket: 'bianluca-pps.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDyMBqpkaB45zbC8I7z0qhAJqSlFFewSYM',
+    authDomain: 'bianluca-pps.firebaseapp.com',
+    messagingSenderId: '123281409180',
+  },
   production: true
 };
