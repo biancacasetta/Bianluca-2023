317f2cc5e6ea14ce6e20b2dd570820225e3857f3
Luca <nahuquilmes2000@gmail.com>
2023-06-16 22:35:27 -0300

    Acualizacion de dueno-supervisor y font splash

--- src/app/servicios/firebase.service.ts
+++ src/app/servicios/firebase.service.ts
@@ -13,4 +13,22 @@ export class FirebaseService {
     const coleccion = this.angularFirestore.collection<any>(nombreColeccion);
     return coleccion.valueChanges();
   }
+
+  agregarDocumento(dato:any,nombreColeccion:string)
+  {
+    return this.angularFirestore.collection(nombreColeccion).add(dato);
+  }
+
+  eliminarDocumento(datoAEliminar:any,nombreColeccion:string)
+  {
+    this.angularFirestore
+    .doc<any>(`${nombreColeccion}/${datoAEliminar.id}`)
+    .delete()
+    .then(() =>{
+      console.log(`Se elimino ${datoAEliminar.apellido} de la lista ${nombreColeccion}`);
+    })
+    .catch((error) =>{
+      console.log(error.code);
+    })
+  }
 }
