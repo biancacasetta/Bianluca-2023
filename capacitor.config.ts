be68004e2a6b3aa6f09ebd8823d7ab75b20ffce9
Luca <nahuquilmes2000@gmail.com>
2023-06-23 19:31:07 -0300

    Configuración splash android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.bianluca.pps',
-  appName: 'La_Comanda_PPS',
+  appName: 'Bianluca',
   webDir: 'www',
   server: {
     androidScheme: 'https'
